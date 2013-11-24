0c1c64fbd17c19def05430465bd077e5bbdaa7e8
Samuel Coelho <samuelfcmc@gmail.com>
2013-11-24 17:14:29 +0000

    Implemented the service to buy a product

--- src/main/java/com/sirs/mobilecashserver/rest/BuyService.java
+++ src/main/java/com/sirs/mobilecashserver/rest/BuyService.java
@@ -7,16 +7,39 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.sirs.mobilecashserver.db.MobileCashServerDB;
+import com.sirs.mobilecashserver.rest.models.BankAccount;
 import com.sirs.mobilecashserver.rest.models.Payment;
+import com.sirs.mobilecashserver.rest.models.PaymentResponse;
+import com.sirs.mobilecashserver.rest.models.Product;
+import com.sirs.mobilecashserver.rest.models.User;
 
-@Path("api")
+@Path("buy")
 public class BuyService {
 
+	private final MobileCashServerDB db = MobileCashServerDB.getInstance();
+
 	@POST
-	// @Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public String buy(Payment payment) {
-		return "ok";
+	@Consumes({ MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_JSON })
+	public PaymentResponse buy(Payment payment) {
+		User user = db.login(payment.getUsername(), payment.getPassword());
+
+		if (user != null) {
+			Product product = db.getProduct(payment.getProduct());
+			BankAccount account = db.getAccount(payment.getUsername());
+			// Check if the user can buy this product
+			if (account.getBalance() >= product.getPrice()) {
+				double oldBalance = account.getBalance();
+				double newBalance = oldBalance - product.getPrice();
+				account.setBalance(newBalance);
+				return new PaymentResponse(user.getUsername(),
+						product.getCode(), account.getBalance());
+			}
+
+		}
+
+		return null;
 	}
 
 	@GET
