2869c44ed56f5054ba5ef098b60bdaba223737a4
mvpgomes <mvp.gomes1989@gmail.com>
2013-12-11 16:02:15 +0000

    Added endpoint to receive the public key

---
+++ src/main/java/com/sirs/mobilecashserver/rest/models/PublicKeyResponse.java
@@ -0,0 +1,46 @@
+package com.sirs.mobilecashserver.rest.models;
+
+/**
+ * The Class AvailabilityResponse.
+ */
+public class PublicKeyResponse extends Response {
+
+    /** The message. */
+    String message;
+
+    /**
+     * Instantiates a new availability response.
+     */
+    public PublicKeyResponse() {
+        super();
+    }
+
+    /**
+     * Instantiates a new availability response.
+     * 
+     * @param message the message
+     */
+    public PublicKeyResponse(String message) {
+        super();
+        this.message = message;
+    }
+
+    /**
+     * Gets the message.
+     * 
+     * @return the message
+     */
+    public String getMessage() {
+        return message;
+    }
+
+    /**
+     * Sets the message.
+     * 
+     * @param message the new message
+     */
+    void setMessage(String message) {
+        this.message = message;
+    }
+
+}
