443346878fb15accddc88f219ed7271c4af4721e
Guilherme Ferreira <guilhermef1.8@gmail.com>
2013-12-11 10:38:09 +0000

    Hello end point returns current date time

--- src/main/java/com/sirs/mobilecashserver/rest/Hello.java
+++ src/main/java/com/sirs/mobilecashserver/rest/Hello.java
@@ -4,18 +4,22 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-/**
- * Hello world!
- * 
- */
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
 @Path("/hello")
 public class Hello {
 	@GET
 	public Response getMsg() {
 
-		String output = "Jersey say : Hello !!!";
+		DateTime dt = new DateTime();
+		DateTimeFormatter fmt = DateTimeFormat
+				.forPattern("dd/MM/yyyy HH:mm:ss");
+		String output = "Jersey say : Hello !!! ";
+
+		output += "at " + fmt.print(dt);
 
 		return Response.status(200).entity(output).build();
 	}
-
 }
